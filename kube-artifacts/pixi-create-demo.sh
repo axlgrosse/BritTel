25ee1cfc454951f92f3428cc22979110ec384701
Axl <axel.grosse@42crunch.com>
2024-01-31 11:15:20 +0100

    adjusted to HTTP

--- kube-artifacts/pixi-create-demo.sh
+++ kube-artifacts/pixi-create-demo.sh
@@ -16,14 +16,15 @@ fi
 kubectl create namespace $RUNTIME_NS
 # Create secrets
 echo "===========> Creating Secrets"
-kubectl create --namespace=$RUNTIME_NS secret tls firewall-certs --key ./etc/tls/private.key --cert ./etc/tls/fullchain-cert-with-ca.pem
+ #kubectl create --namespace=$RUNTIME_NS secret tls firewall-certs --key ./etc/tls/private.key --cert ./etc/tls/fullchain-cert-with-ca.pem
+
 kubectl create --namespace=$RUNTIME_NS secret generic generic-pixi-protection-token --from-env-file='./etc/secret-protection-token'
 # Config Map creation
 echo "===========> Creating ConfigMap"
 kubectl create --namespace=$RUNTIME_NS configmap firewall-props --from-env-file='./etc/deployment.properties'
 # Deployment (Required App/DB + storage)
-echo "===========> Deploying unsecured pixi and database"
-kubectl apply --namespace=$RUNTIME_NS -f pixi-basic-deployment.yaml
+# echo "===========> Deploying unsecured pixi and database"
+# kubectl apply --namespace=$RUNTIME_NS -f pixi-basic-deployment.yaml
 # Deployment (Pixi + FW)
 echo "===========> Deploying secured API firewall"
 kubectl apply --namespace=$RUNTIME_NS -f pixi-secured-deployment.yaml
